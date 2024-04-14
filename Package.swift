5af482bc96f571d2ae84852088558925a57b5ece
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2024-04-14 20:18:14 +0200

    bump spm

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "MCEmojiPicker",
     defaultLocalization: "en",
-    platforms: [.iOS("11.1")],
+    platforms: [.iOS("11.1"), .macOS(.v14)],
     products: [.library(name: "MCEmojiPicker", targets: ["MCEmojiPicker"])],
     dependencies: [],
     targets: [.target(
